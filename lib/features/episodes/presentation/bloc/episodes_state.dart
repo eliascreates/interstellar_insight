c9a6233ade3141eb01ca9069b08ca1c0e75eedce
Elias Kekana <thapeloelias.kekana@gmail.com>
2023-10-04 18:21:29 +0200

    chore: add controller implementation for the episode feature

--- lib/features/episodes/presentation/bloc/episodes_state.dart
+++ lib/features/episodes/presentation/bloc/episodes_state.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'episodes_bloc.dart';
 
 enum EpisodeStatus { initial, loading, success, failure }
@@ -12,6 +13,22 @@ class EpisodesState extends Equatable {
   final EpisodeStatus status;
   final String? errorMessage;
 
+  EpisodesState copyWith({
+    List<Episode>? episodes,
+    EpisodeStatus? status,
+    String? errorMessage,
+  }) {
+    return EpisodesState(
+      episodes: episodes ?? this.episodes,
+      status: status ?? this.status,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [
+        episodes,
+        status,
+        errorMessage,
+      ];
 }
