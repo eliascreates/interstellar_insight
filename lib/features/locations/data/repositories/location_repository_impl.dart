f7bf89d132f90559404ac2774f0fde501ce1ea4e
Elias Kekana <thapeloelias.kekana@gmail.com>
2023-10-05 15:21:35 +0200

    chore: implement the remote data source for locations feature

--- lib/features/locations/data/repositories/location_repository_impl.dart
+++ lib/features/locations/data/repositories/location_repository_impl.dart
@@ -3,6 +3,8 @@ import 'package:interstellar_insight/core/errors/failure.dart';
 import 'package:interstellar_insight/features/locations/data/datasources/remote_location_datasource.dart';
 import 'package:interstellar_insight/features/locations/domain/domain.dart';
 
+import '../mapper/location_mapper.dart';
+
 class LocationRepositoryImpl implements LocationRepository {
   final RemoteLocationDatasource remoteDatasource;
 
@@ -11,12 +13,36 @@ class LocationRepositoryImpl implements LocationRepository {
   @override
   Future<Either<Failure, List<Location>>> getAllLocations() async {
     // TODO: implement getAllLocations
-    throw UnimplementedError();
+    try {
+      final locations = await remoteDatasource.getAllLocations();
+      final locationsEntity =
+          locations.map((location) => toEntity(location)).toList();
+
+      return Right(locationsEntity);
+    } catch (_) {
+      return const Left(
+        ServerFailure(
+          message: 'unable to fetch locations. Try again layer',
+        ),
+      );
+    }
   }
 
   @override
-  Future<Either<Failure, Location>> getLocationById(String id) {
+  Future<Either<Failure, Location>> getLocationById(String id) async {
     // TODO: implement getLocationById
-    throw UnimplementedError();
+    try {
+      final location = await remoteDatasource.getLocationById(id);
+
+      final locationEntity = toEntity(location);
+
+      return Right(locationEntity);
+    } catch (_) {
+      return Left(
+        ServerFailure(
+          message: 'unable to get location with id $id. Try again layer',
+        ),
+      );
+    }
   }
 }
